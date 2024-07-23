c50590f6b40c560313dbc3411ad5abf16502b1b7
Egorrrka1613 <reutov622@gmail.com>
2024-07-23 14:15:18 +0300

    fix 6

--- recursion/6.py
+++ recursion/6.py
@@ -1,26 +1,26 @@
-def print_only_not_even_index(input_list: list, print_index=1):
+def print_only_even_index(input_list: list, print_index=0):
     if len(input_list) > 0 and print_index < len(input_list):
         print(input_list[print_index])
-        print_only_not_even_index(input_list, print_index + 2)
+        print_only_even_index(input_list, print_index + 2)
 
 
 def test(capsys):
-    print_only_not_even_index([])
+    print_only_even_index([])
     captured = capsys.readouterr()
     assert captured.out == ""
 
-    print_only_not_even_index([1])
+    print_only_even_index([1])
     captured = capsys.readouterr()
-    assert captured.out == ""
+    assert captured.out == "1\n"
 
-    print_only_not_even_index([2, 3, 2])
+    print_only_even_index([2, 3, 2])
     captured = capsys.readouterr()
-    assert captured.out == "3\n"
+    assert captured.out == "2\n2\n"
 
-    print_only_not_even_index([0, 1, 2, 3, 4, 5])
+    print_only_even_index([0, 1, 2, 3, 4, 5])
     captured = capsys.readouterr()
-    assert captured.out == "1\n3\n5\n"
+    assert captured.out == "0\n2\n4\n"
 
-    print_only_not_even_index([0, 1, 2, 3, 4, 5, 6])
+    print_only_even_index([0, 1, 2, 3, 4, 5, 6])
     captured = capsys.readouterr()
-    assert captured.out == "1\n3\n5\n"
+    assert captured.out == "0\n2\n4\n6\n"
