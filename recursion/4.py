d3bdbaf450ca398bdd7ff9ad6a7ace2448f99ff0
Egorrrka1613 <reutov622@gmail.com>
2024-07-26 10:38:43 +0300

    final refactor 4

--- recursion/4.py
+++ recursion/4.py
@@ -1,12 +1,14 @@
-def is_palindrome(out_str: str):
-    def recursion(inner_str: str, start_index, end_index):
-        if start_index >= end_index:
-            return True
-        if inner_str[start_index] != inner_str[end_index]:
-            return False
-        return recursion(inner_str, start_index + 1, end_index - 1)
+def recursion(inner_str: str, start_index, end_index):
+    if start_index >= end_index:
+        return True
+    if inner_str[start_index] != inner_str[end_index]:
+        return False
+    return recursion(inner_str, start_index + 1, end_index - 1)
 
-    return recursion(out_str.lower().replace(" ", ""), 0, len(out_str) - 1)
+
+def is_palindrome(out_str: str):
+    format_str = out_str.lower().replace(" ", "")
+    return recursion(format_str, 0, len(format_str) - 1)
 
 
 def test():
