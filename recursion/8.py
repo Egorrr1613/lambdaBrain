dd7ce82db4a9d888a39c368b54f5f89e72d3fef5
Egorrrka1613 <reutov622@gmail.com>
2024-08-02 14:46:34 +0300

    edit 8

--- recursion/8.py
+++ recursion/8.py
@@ -1,56 +1,25 @@
 import os
 
 
-def find_files(
-    current_dir: str,
-    count_files: list,
-    not_check_file_and_dir_in_current_dir: list,
-    deep_storage: list,
-) -> list:
-    # Если не проверенных объектов = 0 и размер deep_storage = 0 (начальная точка)
-    # Значит все обошли, возвращаем кол-во файлов
-    if len(not_check_file_and_dir_in_current_dir) == 0 and len(deep_storage) == 0:
-        return count_files
+def find_files(current_dir: str) -> list:
+    if os.path.isfile(current_dir):
+        return [current_dir]
 
-    # Если не проверенных объектов = 0 и размер deep_storage > 1
-    # Значит на этом уровне все проверено, возвращаемся на директорию выше
-    if len(not_check_file_and_dir_in_current_dir) == 0 and len(deep_storage) > 0:
-        dir_to_rec = os.path.join(current_dir, "..")
-        list_to_rec = deep_storage.pop()
-
-    # Если не проверенных объектов больше чем 0 и очередной объект это файл - то ...
-    if len(not_check_file_and_dir_in_current_dir) > 0 and os.path.isfile(
-        os.path.join(current_dir, not_check_file_and_dir_in_current_dir[0])
-    ):
-        count_files.append(not_check_file_and_dir_in_current_dir.pop(0))
-
-        dir_to_rec = current_dir
-        list_to_rec = not_check_file_and_dir_in_current_dir
-
-    # Если не проверенных объектов больше чем 0 и очередной объект это директория - то ...
-    elif len(not_check_file_and_dir_in_current_dir) > 0 and os.path.isdir(
-        os.path.join(current_dir, not_check_file_and_dir_in_current_dir[0])
-    ):
-
-        dir_to_rec = os.path.join(current_dir, not_check_file_and_dir_in_current_dir[0])
-        list_to_rec = os.listdir(
-            os.path.join(current_dir, not_check_file_and_dir_in_current_dir.pop(0))
-        )
-
-        deep_storage.append(not_check_file_and_dir_in_current_dir)
-
-    return find_files(dir_to_rec, count_files, list_to_rec, deep_storage)
+    result = []
+    for i in os.listdir(current_dir):
+        result += find_files(os.path.join(current_dir, i))
+    return result
 
 
 def test():
     test_dir = os.path.join(os.getcwd(), "..", "data", "8")
-    assert sorted(find_files(test_dir, [], os.listdir(test_dir), [])) == sorted(
+    assert sorted(find_files(test_dir)) == sorted(
         [
-            "i1.py",
-            "i2.py",
-            "i3.py",
-            "i4.py",
-            "i5.py",
-            "42",
+            "/Users/egorreutov/learn/lambdaBrain/recursion/../data/8/a/i1.py",
+            "/Users/egorreutov/learn/lambdaBrain/recursion/../data/8/b/c/i2.py",
+            "/Users/egorreutov/learn/lambdaBrain/recursion/../data/8/d/e/f/i3.py",
+            "/Users/egorreutov/learn/lambdaBrain/recursion/../data/8/d/i4.py",
+            "/Users/egorreutov/learn/lambdaBrain/recursion/../data/8/i5.py",
+            "/Users/egorreutov/learn/lambdaBrain/recursion/../data/8/42",
         ]
     )
