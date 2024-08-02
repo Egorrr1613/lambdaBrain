c51191ef96d305b4a564cf1b0dbf8e0eb68ac7d2
Egorrrka1613 <reutov622@gmail.com>
2024-08-02 21:55:03 +0300

    add N1 in 28 task

---
+++ 28_task/1.py
@@ -0,0 +1,19 @@
+def squirrel(n: int) -> int:
+    if n == 0:
+        return 0
+
+    def fact(input_int: int) -> int:
+        if input_int == 1:
+            return 1
+        else:
+            return input_int * (fact(input_int - 1))
+
+    return int(str(fact(n))[0])
+
+
+def test():
+    assert squirrel(0) == 0
+    assert squirrel(1) == 1
+    assert squirrel(4) == 2
+    assert squirrel(14) == 8
+
