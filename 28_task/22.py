1c81df9dceb174365865256ea37026724e557e42
Egorrrka1613 <reutov622@gmail.com>
2024-09-24 10:30:56 +0300

    fix 22

--- 28_task/22.py
+++ 28_task/22.py
@@ -5,11 +5,10 @@ def SherlockValidString(s: str) -> bool:
             d_char_count[char] = 0
         d_char_count[char] += 1
 
-    d_count_value: dict[int, list] = {}
+    d_count_value: dict[int, list[str]] = {}
     for k, v in d_char_count.items():
         if d_count_value.get(v) is None:
-            d_count_value[v] = [0]
-        d_count_value[v][0] += 1
+            d_count_value[v] = []
         d_count_value[v].append(k)
 
     d_count_value_len = len(d_count_value)
@@ -17,20 +16,37 @@ def SherlockValidString(s: str) -> bool:
         return True
     if d_count_value_len != 2:
         return False
+
     items = list(d_count_value.items())
-    if items[0][1] > items[1][1]:
-        max_key, min_key = items[0], items[1]
+    if len(items[0][1]) > len(items[1][1]):
+        key_max, key_min = items[0][0], items[1][0]
     else:
-        max_key, min_key = items[1], items[0]
-    if min_key[0] - 1 == 0 or min_key[0] - 1 == max_key[0]:
+        key_max, key_min = items[1][0], items[0][0]
+
+    if (
+        len(d_count_value[key_min]) == 1
+        and d_char_count[d_count_value[key_min][0]] - 1
+        == d_char_count[d_count_value[key_max][0]]
+    ):
         return True
+    if (
+        len(d_count_value[key_min]) == 1
+        and d_char_count[d_count_value[key_min][0]] - 1 == 0
+    ):
+        return True
+    if (
+        len(d_count_value[key_max]) == 1
+        and d_char_count[d_count_value[key_max][0]] - 1 == 0
+    ):
+        return True
+
     return False
 
 
 def test():
     assert SherlockValidString("xyz") is True
     assert SherlockValidString("xx") is True
-    assert SherlockValidString("xxyx") is False
+    assert SherlockValidString("xxyx") is True
     assert SherlockValidString("xy") is True
     assert SherlockValidString("xyzz") is True
     assert SherlockValidString("xyyzz") is True
@@ -41,4 +57,5 @@ def test():
     assert SherlockValidString("xxxyyzzzzzz") is False
     assert SherlockValidString("zzzzzz") is True
     assert SherlockValidString("zzzzzzx") is True
+    assert SherlockValidString("xxyyza") is False
 
