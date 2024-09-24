242881c30bfaf3a7dcc4bf9d1127f0b86e6814fb
Egorrrka1613 <reutov622@gmail.com>
2024-09-24 13:17:52 +0300

    fix 22

--- 28_task/22.py
+++ 28_task/22.py
@@ -5,38 +5,43 @@ def SherlockValidString(s: str) -> bool:
             d_char_count[char] = 0
         d_char_count[char] += 1
 
-    d_count_value: dict[int, list[str]] = {}
+    d_value_char: dict[int, list[str]] = {}
     for k, v in d_char_count.items():
-        if d_count_value.get(v) is None:
-            d_count_value[v] = []
-        d_count_value[v].append(k)
+        if d_value_char.get(v) is None:
+            d_value_char[v] = []
+        d_value_char[v].append(k)
 
-    d_count_value_len = len(d_count_value)
+    d_count_value_len = len(d_value_char)
     if d_count_value_len == 1:
         return True
     if d_count_value_len != 2:
         return False
 
-    items = list(d_count_value.items())
+    items = list(d_value_char.items())
     if len(items[0][1]) > len(items[1][1]):
         key_max, key_min = items[0][0], items[1][0]
     else:
         key_max, key_min = items[1][0], items[0][0]
 
     if (
-        len(d_count_value[key_min]) == 1
-        and d_char_count[d_count_value[key_min][0]] - 1
-        == d_char_count[d_count_value[key_max][0]]
+        len(d_value_char[key_min]) == 1
+        and d_char_count[d_value_char[key_min][0]] - 1
+        == d_char_count[d_value_char[key_max][0]]
     ):
         return True
     if (
-        len(d_count_value[key_min]) == 1
-        and d_char_count[d_count_value[key_min][0]] - 1 == 0
+        len(d_value_char[key_max]) == 1
+        and d_char_count[d_value_char[key_max][0]] - 1
+        == d_char_count[d_value_char[key_min][0]]
     ):
         return True
+
     if (
-        len(d_count_value[key_max]) == 1
-        and d_char_count[d_count_value[key_max][0]] - 1 == 0
+        len(d_value_char[key_min]) == 1
+        and d_char_count[d_value_char[key_min][0]] - 1 == 0
+    ) or (
+        len(d_value_char[key_max]) == 1
+        and d_char_count[d_value_char[key_max][0]] - 1 == 0
     ):
         return True
 
@@ -58,4 +63,6 @@ def test():
     assert SherlockValidString("zzzzzz") is True
     assert SherlockValidString("zzzzzzx") is True
     assert SherlockValidString("xxyyza") is False
-
+    assert SherlockValidString("xxyyy") is True
+    assert SherlockValidString("xxxyy") is True
+    assert SherlockValidString("xxxxxyyyyyy") is True
