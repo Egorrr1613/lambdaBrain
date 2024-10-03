880003cad989bf47d716b883a5750bbceaa0cc0b
Egorrrka1613 <reutov622@gmail.com>
2024-10-03 12:40:36 +0300

    25 del recursion

--- 28_task/25.py
+++ 28_task/25.py
@@ -1,10 +1,10 @@
 def TransformTransform(a: list[int], _: int) -> bool:
     b = transform(a)
     b_after_first_transform = transform(b)
-    s = get_sum(b_after_first_transform, 0, len(b_after_first_transform))
-    if s == 0 or s % 2 == 0:
-        return True
-    return False
+    s = sum(b_after_first_transform)
+    if s % 2 != 0:
+        return False
+    return True
 
 
 def transform(a):
@@ -19,12 +19,6 @@ def transform(a):
     return b
 
 
-def get_sum(list_l: list[int], index: int, n: int):
-    if index == n - 1:
-        return list_l[index]
-    return list_l[index] + get_sum(list_l, index + 1, n)
-
-
 def get_max(list_l: list[int], index: int, n: int, result: int) -> int:
     if index == n:
         return result
@@ -34,10 +28,10 @@ def get_max(list_l: list[int], index: int, n: int, result: int) -> int:
 
 
 def test():
-    assert get_sum([1, 2, 3], 0, 3) == 6
-    assert get_sum([1], 0, 1) == 1
     assert TransformTransform([9, 9, 9, 9, 9], 5)
     assert TransformTransform([2, 3, 4, 5, 6, 7, 8, 9], 8)
+    long_arr = [1, 2, 1, 7, 2, 4, 3, 1, 5, 1, 2, 1, 6, 1, 2]
+    assert TransformTransform(long_arr, len(long_arr)) is False
     assert TransformTransform([3, 3], 2)
     assert transform([3, 3]) == [3, 3, 3]
     a = transform([3, 2, 1])
