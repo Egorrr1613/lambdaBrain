ecfeb27cf797949e4b66441983b15310fd1446c8
Egorrrka1613 <reutov622@gmail.com>
2024-11-03 16:00:01 +0300

    add 5

---
+++ 8_task/5/5.py
@@ -0,0 +1,33 @@
+def massdriver(activate: list[int]) -> int:
+    result = -1
+    buffer: dict[int: list[int: int]] = {}
+    for i, v in enumerate(activate):
+
+        find_duplicate_num = False
+        buffer_data = buffer.get(v)
+        if buffer_data is None:
+            buffer[v] = [i, 0]
+            continue
+
+        if buffer_data[1] == 0:
+            buffer[v] = [buffer[v][0], i]
+            find_duplicate_num = True
+
+        if find_duplicate_num and result == -1:
+            result = v
+            continue
+
+        if find_duplicate_num:
+            result = v if buffer[v][0] < buffer[result][0] else result
+
+    if result == -1:
+        return result
+    return buffer[result][0]
+
+
+def test():
+    assert massdriver([1, 2]) == -1
+    assert massdriver([1, 1, 1]) == 0
+    assert massdriver([1, 2, 3, 1, 2, 3, 4]) == 0
+    assert massdriver([1, 2, 3, 4, 5, 6, 7]) == -1
+    assert massdriver([1, 2, 3, 4, 3, 4, 2]) == 1
