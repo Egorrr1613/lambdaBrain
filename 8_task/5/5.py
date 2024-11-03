8abebfbf4df0732fd441b4dc8a2964e13c58d36f
Egorrrka1613 <reutov622@gmail.com>
2024-11-03 16:03:47 +0300

    more test to 5

--- 8_task/5/5.py
+++ 8_task/5/5.py
@@ -31,3 +31,5 @@ def test():
     assert massdriver([1, 2, 3, 1, 2, 3, 4]) == 0
     assert massdriver([1, 2, 3, 4, 5, 6, 7]) == -1
     assert massdriver([1, 2, 3, 4, 3, 4, 2]) == 1
+    assert massdriver([9, 1, 2, 3, 4, 3, 4, 2, 9]) == 0
+    assert massdriver([1, 2, 3, 4, 5, 6, 7, 7]) == 6
