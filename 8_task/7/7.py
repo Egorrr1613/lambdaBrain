dcfc99e1fec3e812765352d066e4ee99418a21e7
Egorrrka1613 <reutov622@gmail.com>
2024-11-10 18:55:15 +0300

    add 7

--- 8_task/7/7.py
+++ 8_task/7/7.py
@@ -1,7 +1,6 @@
 def matrix(n: int, m: int, input_matrix: list[list[int]]) -> list[int]:
     res = []
     recursion(n, m, input_matrix, 0, 0, res)
-    print(res)
     return res
 
 
@@ -13,48 +12,22 @@ def recursion(n: int, m: int, input_matrix: list[list[int]],
         return result
 
     for i in range(n * m):
-        ii = i + 1
+        increment_i = i + 1
         if i < n:
             result.append(input_matrix[start_point_m][i + start_point_n])
-            print('a')
 
-        elif ii < n + m:
-            print(f'b: i: {i}, n: {n}, m: {m}, start_point_m: {start_point_m}'
-                  f' coordinate: {i - (m - 1) + start_point_m}')
+        elif increment_i < n + m:
             result.append(input_matrix[i - (n - 1) + start_point_m][n - 1 + start_point_n])
 
-        elif ii <= m - 2 + 2 * n:
+        elif increment_i <= m - 2 + 2 * n:
             result.append(input_matrix[m - 1 + start_point_m]
                           [(m - 2 + 2 * n) - i - 1 + start_point_n])
-            print('c')
 
-        elif ii <= 2 * (m - 2) + 2 * n:
+        elif increment_i <= 2 * (m - 2) + 2 * n:
             result.append(input_matrix[(2 * (m - 2) + 2 * n) - i][start_point_n])
-            print('d')
 
-        print(f'i: {i}, res: {result}\n')
-
-        if ii == (m - 2) * 2 + 2 * n:
+        if increment_i == (m - 2) * 2 + 2 * n:
             return recursion(n - 2, m - 2, input_matrix, start_point_m + 1, start_point_n + 1, result)
-
-
-"""
-    n -- ширина, m-высота
-    
-    Перешли на очередной индекс массива
-    Если идекс + 1 меньше или равен чем: ширина, тогда добавляем в массив элемент из 0[i]
-    
-    Если индекс + 1 меньше чем: ширина + высота, то вычитаем из индекса ширина, получаем h (индекс нужного массива)
-        добавляем элемент по индексу h[m]
-    
-    Если индекс меньше или равен: высота - 2 + 2*ширина (n - 2 + 2m), 
-        тогда вычитаем из индекса 2*ширина + высоту - 2 (n - 2 + 2m). Модуль разности будет используем как n[res - i]
-        
-    Если индекс меньше или равен (высота - 2)*2 + 2*ширина (2(n - 2) + 2m), 
-        тогда вычитаем из индекса 2*ширина + 2*(высоту - 2) (2(n - 2) + 2m). Модуль разности будет используем как res[0]
-        
-    Иначе нужно сужать ось
-"""
 
 
 def test():
