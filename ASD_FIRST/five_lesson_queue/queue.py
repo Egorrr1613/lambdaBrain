b37de4ef9b87a71fddb67a0474062ce8d49c7fc6
Egorrrka1613 <reutov622@gmail.com>
2025-03-03 19:35:23 +0300

    add queue solution

---
+++ ASD_FIRST/five_lesson_queue/queue.py
@@ -0,0 +1,60 @@
+class Node:
+    def __init__(self, v):
+        self.value = v
+        self.prev = None
+        self.next = None
+
+
+class DummyNode(Node):
+    def __init__(self, v):
+        super().__init__(v)
+
+
+class Queue:
+    def __init__(self):
+        self.head = DummyNode(None)
+        self.tail = self.head
+
+        self.head.next = self.tail
+        self.head.prev = self.tail
+
+        self.tail.prev = self.head
+        self.tail.next = self.head
+        self.count_node = 0
+
+    def enqueue(self, value) -> None:
+        new_node = Node(value)
+        new_node.prev = self.tail.prev
+        new_node.next = self.tail
+
+        self.tail.prev.next = new_node
+        self.tail.prev = new_node
+        self.count_node += 1
+
+    def dequeue(self):
+        if self.count_node == 0:
+            return None
+        result = self.head.next.value
+
+        self.head.next.next.prev = self.head
+        self.head.next = self.head.next.next
+
+        self.count_node -= 1
+        return result
+
+    def size(self):
+        return self.count_node
+
+    def print_all_nodes(self) -> None:
+        node = self.head.next
+        while type(node) is Node:
+            print(node.value)
+            node = node.next
+
+    def clean(self) -> None:
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def __len__(self) -> int:
+        return self.count_node
+
