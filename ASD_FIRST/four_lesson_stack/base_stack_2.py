a4138bca0980cb424f3370593f8bf84d2555ec63
Egorrrka1613 <reutov622@gmail.com>
2025-03-04 22:37:50 +0300

    add is_balance expand

--- ASD_FIRST/four_lesson_stack/base_stack_2.py
+++ ASD_FIRST/four_lesson_stack/base_stack_2.py
@@ -3,12 +3,15 @@ from ASD_FIRST.four_lesson_stack.base_stack import Stack
 
 def is_balance(input_str: str):
     stack = Stack()
+    reverse_symbol = {')': '(', '}': '{', ']': '['}
     for i in input_str:
-        if i == '(':
-            stack.push('i')
+        if i in ['(', '{', '[']:
+            stack.push(i)
             continue
-        if i == ')' and stack.size() > 0:
-            stack.pop()
+        if i in [')', '}', ']'] and stack.size() > 0:
+            rm_symbol = stack.pop()
+            if rm_symbol != reverse_symbol[i]:
+                return False
             continue
         return False
 
@@ -17,15 +20,27 @@ def is_balance(input_str: str):
     return False
 
 
-
 def test_is_balance():
     assert is_balance(input_str="()") is True
+    assert is_balance(input_str="()()") is True
     assert is_balance(input_str="(())") is True
     assert is_balance(input_str="(()((())()))") is True
     assert is_balance(input_str="(()((())()))") is True
+    assert is_balance(input_str="") is True
+
     assert is_balance(input_str="(()()(()") is False
     assert is_balance(input_str="())") is False
+    assert is_balance(input_str="(()") is False
+    assert is_balance(input_str="())") is False
     assert is_balance(input_str="))") is False
-    assert is_balance(input_str="") is True
     assert is_balance(input_str="(") is False
+    assert is_balance(input_str="))((") is False
+    assert is_balance(input_str="(()()(()") is False
 
+
+def test_is_balance_v2():
+    assert is_balance(input_str="({})") is True
+    assert is_balance(input_str="({[]})") is True
+    assert is_balance(input_str="[(]{})") is False
+
+    assert is_balance(input_str="{(})") is False
