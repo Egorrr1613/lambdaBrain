f26e6eccd4c434f665e99ab03b6addf740d5480c
Egorrrka1613 <reutov622@gmail.com>
2025-03-13 16:52:12 +0300

    add 4 sol

--- ASD_FIRST/six_lesson_deque/deque_2.py
+++ ASD_FIRST/six_lesson_deque/deque_2.py
@@ -1,3 +1,4 @@
+
 from ASD_FIRST.six_lesson_deque.deque import Deque
 
 
@@ -19,3 +20,71 @@ def is_palindrome(checked_str: str) -> bool:
         if left != right:
             return False
     return True
+
+
+class DequeWithGetMin:
+    """Решение 4 дополнительного задания
+
+        Использую переменную для хранения минимального элемента.
+        Если этот элемент удаляется - находим по внутреннему массиву перебором новый элемент
+    """
+
+    def __init__(self):
+        self.array = []
+        self.min_el = None
+
+    def addFront(self, item):
+        self.array.insert(0, item)
+        self.check_add_element_by_min(item, )
+
+    def addTail(self, item):
+        self.array.insert(self.size(), item)
+        self.check_add_element_by_min(item, )
+
+    def removeFront(self):
+        if self.size() == 0:
+            return None
+        result = self.array.pop(0)
+        self.check_del_element_by_min(result)
+        return result
+
+    def removeTail(self):
+        if self.size() == 0:
+            return None
+        result = self.array.pop(self.size() - 1)
+        self.check_del_element_by_min(result)
+        return result
+
+    def size(self):
+        return len(self.array)
+
+    def min(self):
+        return self.min_el
+
+    def check_add_element_by_min(self, element):
+        if not isinstance(element, int):
+            return
+
+        if self.min_el is None:
+            self.min_el = element
+            return
+        if self.min_el > element:
+            self.min_el = element
+
+    def check_del_element_by_min(self, element):
+        if not isinstance(element, int):
+            return
+        if element < self.min_el:
+            return ('Incorrect state',)
+        if self.size() == 0:
+            self.min_el = None
+            return
+        if element == self.min_el:
+            self.min_el = self.find_min_el()
+
+    def find_min_el(self):
+        res = self.array[0]
+        for i in range(self.size()):
+            if self.array[i] < res:
+                res = self.array[i]
+        return res
--- ASD_FIRST/six_lesson_deque/test_deque_2.py
+++ ASD_FIRST/six_lesson_deque/test_deque_2.py
@@ -1,4 +1,4 @@
-from ASD_FIRST.six_lesson_deque.deque_2 import is_palindrome
+from ASD_FIRST.six_lesson_deque.deque_2 import is_palindrome, DequeWithGetMin
 
 
 def test_palindrome():
@@ -15,3 +15,48 @@ def test_palindrome():
     assert is_palindrome("Я иду с мечем судия") is True
     assert is_palindrome("Я иду с меем судия") is True
     assert is_palindrome("Я иду с мечом судия") is False
+
+
+def test_deq_min():
+    d = DequeWithGetMin()
+    assert d.min() is None
+    d.addTail(1)
+    assert d.min() == 1
+    d.addTail(-5)
+    assert d.min() == -5
+    d.addTail(4)
+    assert d.min() == -5
+    d.removeTail()
+    d.removeTail()
+    assert d.min() == 1
+
+def test_deq_min_2():
+    d = DequeWithGetMin()
+    d.addFront(5)
+    d.addTail(6)
+    d.addFront(3)
+    d.addFront(8)
+    d.addFront(9)
+    d.removeTail()
+    d.removeTail()
+    d.removeTail()
+    assert d.min() == 8
+
+    d = DequeWithGetMin()
+    d.addFront(0)
+    d.addFront(0)
+    d.addFront(1)
+    d.addFront(-5)
+    assert d.min() == -5
+
+    d.removeFront()
+    assert d.min() == 0
+
+    d.removeTail()
+    assert d.min() == 0
+
+    d.removeTail()
+    assert d.min() == 1
+
+    d.removeTail()
+    assert d.min() is None
