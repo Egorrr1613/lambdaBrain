945c1e6a9e73eb0629b789e4fca989380c31876e
Egorrrka1613 <reutov622@gmail.com>
2025-03-28 12:14:37 +0300

    add hash table

---
+++ ASD_FIRST/eight_hash_table/hash_table.py
@@ -0,0 +1,39 @@
+class HashTable:
+    def __init__(self, sz, stp):
+        self.size = sz
+        self.step = stp
+        self.slots = [None] * self.size
+
+    def hash_fun(self, value: str) -> int:
+        return sum(i for i in str.encode(value)) % self.size
+
+    def seek_slot(self, value: str) -> int | None:
+        index = self.hash_fun(value)
+        for _ in range(self.size // self.step + 1):
+            if self.slots[index] is None or self.slots[index] == value:
+                return index
+            index = (
+                index + self.step
+                if index + self.step < self.size
+                else (index + self.step) % self.size
+            )
+        return None
+
+    def put(self, value: str) -> int | None:
+        index = self.seek_slot(value)
+        if index is None:
+            return None
+        self.slots[index] = value
+        return index
+
+    def find(self, value):
+        index = self.hash_fun(value)
+        for _ in range(self.size // self.step + 1):
+            if self.slots[index] == value:
+                return index
+            index = (
+                index + self.step
+                if index + self.step < self.size
+                else (index + self.step) % self.size
+            )
+        return None
