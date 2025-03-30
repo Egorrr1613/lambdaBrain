e5dac145f01de0430e81e39dc9642f14dc872eb2
Egorrrka1613 <reutov622@gmail.com>
2025-03-30 18:51:30 +0300

    push reflection to queue-2.py

--- ASD_FIRST/five_lesson_queue/test_queue_3.py
+++ ASD_FIRST/five_lesson_queue/test_queue_3.py
@@ -1,4 +1,5 @@
 from ASD_FIRST.five_lesson_queue.queue_2 import CycleBufferQueue
+from ASD_FIRST.five_lesson_queue.queue_2 import CycleBufferQueue2
 
 
 def test_cycle_buffer_queue_0():
@@ -67,3 +68,73 @@ def test_cycle_buffer_queue():
 
     assert q.dequeue() == 98
     assert q.dequeue() == 97
+
+def test_cycle_buffer_queue2_0():
+    q = CycleBufferQueue2(1)
+    assert q.count_el == 0
+    assert q.is_full() is False
+    assert q.head_index == 0
+    assert q.tail_index == 0
+    assert q.dequeue() is None
+    q.enqueue(95)
+    q.enqueue(96)
+    q.enqueue(97)
+    assert q.is_full() is True
+    assert q.head_index == 0
+    assert q.tail_index == 1
+    assert q.count_el == 1
+    assert q.dequeue() == 95
+    assert q.is_full() is False
+    assert q.head_index == 1
+    assert q.tail_index == 1
+    assert q.count_el == 0
+
+
+def test_cycle_buffer_queue2():
+    q = CycleBufferQueue2(5)
+    assert q.count_el == 0
+    assert q.head_index == 0
+    assert q.tail_index == 0
+    assert q.is_full() is False
+
+    q.enqueue(99)
+    assert q.tail_index == 1
+    assert q.head_index == 0
+    assert q.count_el == 1
+    assert q.is_full() is False
+
+    q.enqueue(98)
+    assert q.tail_index == 2
+    assert q.head_index == 0
+    assert q.count_el == 2
+
+    assert q.dequeue() == 99
+    assert q.head_index == 1
+    assert q.tail_index == 2
+    assert q.count_el == 1
+
+    q.enqueue(97)
+    assert q.tail_index == 3
+    assert q.head_index == 1
+
+    q.enqueue(96)
+    assert q.tail_index == 4
+    assert q.head_index == 1
+
+    q.enqueue(95)
+    assert q.tail_index == 5
+    assert q.head_index == 1
+
+    q.enqueue(94)
+    assert q.tail_index == 0
+    assert q.head_index == 1
+    assert q.count_el == 5
+    assert q.is_full() is True
+
+    assert q.dequeue() == 98
+    assert q.dequeue() == 97
+    assert q.dequeue() == 96
+    assert q.dequeue() == 95
+    assert q.dequeue() == 94
+    assert q.count_el == 0
+
